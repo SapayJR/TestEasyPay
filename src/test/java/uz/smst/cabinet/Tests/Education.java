b9ceba1d70ead717f5c69f2d510b0cff453ed4ea
Akbar <madara9626@mail.ru>
2018-09-25 15:54:16 +0500

    INHA

--- src/test/java/uz/smst/cabinet/Tests/Education.java
+++ src/test/java/uz/smst/cabinet/Tests/Education.java
@@ -2,7 +2,6 @@ package uz.smst.cabinet.Tests;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.omg.PortableServer.THREAD_POLICY_ID;
 import org.openqa.selenium.By;
 import uz.smst.cabinet.Settings.WebDriverSettings;
 
@@ -29,8 +28,57 @@ public class Education extends WebDriverSettings {
         driver.findElement(By.name("amount")).sendKeys("1");
         Thread.sleep(2000);
 
-        Assert.assertEquals("Обязательное поле",
-                driver.findElement(By.xpath("//div[@class='form-control-feedback ng-tns-c5-6 ng-star-inserted']")).getText());
+        Assert.assertEquals("Минимальная сумма сум.",
+                driver.findElement(By.xpath("/html[1]/body[1]/modal-container[1]/div[1]/div[1]/div[1]/div[1]/div[1]/app-modal[1]/form[1]/div[2]/div[1]/div[2]/div[1]/div[1]")).getText());
+
+        driver.findElement(By.name("amount")).clear();
+        driver.findElement(By.name("amount")).sendKeys("500");
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(3000);
+
+        Assert.assertTrue(isElementPresent(By.xpath("//h3[contains(text(),'Оплата за услугу Университет ИНХА')]")));
+        Assert.assertTrue(isElementPresent(By.xpath("//div[@class='pay-logo']//img[@class='ng-tns-c7-3']")));
+        Assert.assertTrue(isElementPresent(By.xpath("//div[contains(@class,'col-lg-12 pay-logo text-center')]//img[@class='ng-tns-c7-3']")));
+
+        driver.findElement(By.name("cardNumber")).clear();
+        driver.findElement(By.name("cardNumber")).sendKeys("8600510369728358");
+        Thread.sleep(3000);
+
+
+        driver.findElement(By.name("exDate")).clear();
+        driver.findElement(By.name("exDate")).sendKeys("2008");
+
+
+        driver.findElement(By.name("phoneNumber")).clear();
+        driver.findElement(By.name("phoneNumber")).sendKeys("901203445");
+
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(2000);
+
+        Assert.assertEquals("Номер карты не соответствует остальным данным, перепроверьте все данные",
+                driver.findElement(By.xpath("//h3[@class='alert alert-danger']")).getText());
+
+
+        driver.findElement(By.name("cardNumber")).clear();
+        driver.findElement(By.name("cardNumber")).sendKeys("8600510369728378");
+
+
+
+        driver.findElement(By.name("exDate")).clear();
+        driver.findElement(By.name("exDate")).sendKeys("2008");
+
+
+        driver.findElement(By.name("phoneNumber")).clear();
+        driver.findElement(By.name("phoneNumber")).sendKeys("901203445");
+        Thread.sleep(5000);
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(5000);
+
+        Assert.assertEquals("Убедительная просьба проверить все данные, если данные верны то введите код и нажмите продолжить.",
+                driver.findElement(By.xpath("//div[@class='alert alert-info']")).getText());
 
     }
 }
